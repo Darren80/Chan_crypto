5a0dd58093d96f03f09378e5452c1f850c8acef1
Darren80 <we444465@gmail.com>
2018-11-12 15:51:56 +0000

    abc

--- src/App.js
+++ src/App.js
@@ -3,6 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this.fetchResources = this.fetchResources.bind(this);
+  }
+
+  componentDidMount() {
+    let timer1 = setInterval(this.fetchResources, 2000);
+    this.setState({timer1: timer1});
+  }
+
+  fetchResources() {
+    fetch('https://cors-proxy-0.herokuapp.com/https://a.4cdn.org/biz/1.json').then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request Failed!');
+    }, networkError => {
+    console.log(networkError.message);
+    }).then(jsonResponse => {
+      this.setState({ chan: jsonResponse });
+      console.log(jsonResponse);
+    });
+    this.setState({ counter: this.state.counter + 1});
+    console.log(this.state.counter);
+  }
+
   render() {
     return (
       <div className="App">
