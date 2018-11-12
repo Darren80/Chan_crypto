748c821fa90a6c9971d91a47cda6de6ab4e09d2e
Darren80 <we444465@gmail.com>
2018-11-12 21:25:17 +0000

    Updates

---
+++ precache-manifest.fdcd44e6253606cb25db31c49071b2ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Chan_crypto/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92daa3c0ab807467bdf2",
+    "url": "/Chan_crypto/static/js/runtime~main.92daa3c0.js"
+  },
+  {
+    "revision": "ef60642aa5a1c4fd528f",
+    "url": "/Chan_crypto/static/js/main.ef60642a.chunk.js"
+  },
+  {
+    "revision": "252e1990f01ba48f0700",
+    "url": "/Chan_crypto/static/js/1.252e1990.chunk.js"
+  },
+  {
+    "revision": "ef60642aa5a1c4fd528f",
+    "url": "/Chan_crypto/static/css/main.37e204f7.chunk.css"
+  },
+  {
+    "revision": "24dccc139126aa67e0991c266f43a0a8",
+    "url": "/Chan_crypto/index.html"
+  }
+];
