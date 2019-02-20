8175b8650eb97e65a44029182b7d14da23f6a9f8
Darren80 <we444465@gmail.com>
2019-02-20 15:52:13 +0000

    build

---
+++ build/precache-manifest.fb2343394a5c2469f77e79dfe5b821fd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1a2d01bcd034270a3bc92176edfb66c",
+    "url": "/Chan_crypto/static/media/addNodeIcon.a1a2d01b.png"
+  },
+  {
+    "revision": "37dc9e633d7a473d5ec8",
+    "url": "/Chan_crypto/static/css/main.57c3c09d.chunk.css"
+  },
+  {
+    "revision": "32e55f1080d9a50aeb430e767fd5d65e",
+    "url": "/Chan_crypto/static/media/1024px-No_image_available.svg.32e55f10.png"
+  },
+  {
+    "revision": "7ac1d88a7c2b54f33743",
+    "url": "/Chan_crypto/static/js/1.7ac1d88a.chunk.js"
+  },
+  {
+    "revision": "92daa3c0ab807467bdf2",
+    "url": "/Chan_crypto/static/js/runtime~main.92daa3c0.js"
+  },
+  {
+    "revision": "dd0baa69a69937600dc3bf035f3873c4",
+    "url": "/Chan_crypto/static/media/backIcon.dd0baa69.png"
+  },
+  {
+    "revision": "260c9c6535065674bb5e59a73f894abb",
+    "url": "/Chan_crypto/static/media/cross.260c9c65.png"
+  },
+  {
+    "revision": "37dc9e633d7a473d5ec8",
+    "url": "/Chan_crypto/static/js/main.37dc9e63.chunk.js"
+  },
+  {
+    "revision": "d5422321c4f6feed4081891051f9a6b2",
+    "url": "/Chan_crypto/static/media/editIcon.d5422321.png"
+  },
+  {
+    "revision": "d5267b8db2498e44bd567bd12e07576b",
+    "url": "/Chan_crypto/static/media/connectIcon.d5267b8d.png"
+  },
+  {
+    "revision": "02d321edc6e03aea1675001899d8f479",
+    "url": "/Chan_crypto/static/media/deleteIcon.02d321ed.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Chan_crypto/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ac1d88a7c2b54f33743",
+    "url": "/Chan_crypto/static/css/1.53aecea1.chunk.css"
+  },
+  {
+    "revision": "75d9f35dbca8d2d2098cf865d40c70d6",
+    "url": "/Chan_crypto/index.html"
+  }
+];
