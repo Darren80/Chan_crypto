c17c3894e277e771bc875f77003e7503181d53d5
Darren80 <we444465@gmail.com>
2019-05-27 22:58:13 +0100

    Auto Push

--- Server/express/express-server.js
+++ Server/express/express-server.js
@@ -47,18 +47,18 @@ app.use(require('./routes'));
 
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Expose-Headers', 'Content-Length');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-  if (req.method === 'OPTIONS') {
-    return res.send(200);
-  } else {
-    return next();
-  }
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//   res.header('Access-Control-Expose-Headers', 'Content-Length');
+//   res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+//   if (req.method === 'OPTIONS') {
+//     return res.send(200);
+//   } else {
+//     return next();
+//   }
+// });
 
 app.post('/compress', async (req, res, next) => {
 
--- Server/express/generalUse.js
+++ Server/express/generalUse.js
@@ -1,12 +1,23 @@
-
 const { app, owaspApp } = require("./express-server");
 
 const compression = require('compression');
 const bodyParser = require("body-parser");
 
-
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+        return res.send(200);
+    } else {
+        return next();
+    }
+});
 
 owaspApp.use(compression());
